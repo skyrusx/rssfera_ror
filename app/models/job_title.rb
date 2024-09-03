56cec554e388a9faa048accb73e3226045920c91
Ruslan Fedotov <progruson@gmail.com>
2024-09-03 10:09:54 +0700

    Fix job_title

--- app/models/job_title.rb
+++ app/models/job_title.rb
@@ -1,6 +1,8 @@
 class JobTitle < ApplicationRecord
   has_many :team_member_job_title_joins
   has_many :team_members, through: :team_member_job_title_joins, source: :team_member
+  has_many :vacancy_employment_type_joins
+  has_many :vacancies, through: :vacancy_employment_type_joins, source: :vacancy
   has_many :vacancies
 
   scope :active, -> { where(status: true) }
