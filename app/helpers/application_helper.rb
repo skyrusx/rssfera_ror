c49f7a69ce44105899ef15d363a41224284e9328
Ruslan Fedotov <progruson@gmail.com>
2024-09-09 13:21:22 +0700

    Fix ApplicationHelper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,30 @@
 module ApplicationHelper
   include Application::HtmlHelper
+
+  def footer_data
+    { contacts: Contact.last, social_links: SocialLink.active }
+  end
+
+  def meta_data
+    Setting.find_by(page: controller_name)
+  end
+
+  def link_active_class(link_path)
+    page_fullpath = request.fullpath
+    if params[:slug].present?
+      page_fullpath.gsub!("/#{params[:slug]}", "")
+    end
+    page_fullpath == link_path ? "nav-item active" : "nav-item"
+  end
+
+  def pagination(collection, anchor)
+    params = { anchor: "new-objects" }
+    will_paginate(
+      collection,
+      params: params,
+      renderer: WillPaginate::ActionView::BootstrapLinkRenderer,
+      previous_label: "&laquo;",
+      next_label: "&raquo;"
+    )
+  end
 end
