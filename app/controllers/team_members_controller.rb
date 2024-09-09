cb926f7e7f0251205c254bbc9b29d3eb3bc4f5f2
Ruslan Fedotov <progruson@gmail.com>
2024-09-09 13:11:36 +0700

    Fix TeamMembersController

--- app/controllers/team_members_controller.rb
+++ app/controllers/team_members_controller.rb
@@ -1,12 +1,31 @@
 class TeamMembersController < ApplicationController
-  add_breadcrumb I18n.t("admin_breadcrumbs.team_members"), :team_members_path
+  include ApplicationHelper
 
   def index
-    @contacts = Contact.last
-    @social_links = SocialLink.active
-    @team_members = TeamMember.active.sort_by { |street| street.position }
+    add_breadcrumb meta_data.page_name, :team_members_path
+
+    @team_members = TeamMember.active.sort_by { |team_member| team_member.position }
   end
 
   def show
+    @team_member = TeamMember.find_by_slug(params[:slug])
+
+    @realties = @team_member.realties.active.group_by(&:type_object).to_h
+    count_all_objects = @realties.map { |_, values| values.size }.sum
+    content_objects = @realties.map { |_, values| values }.flatten
+    @realty_data = {
+      tabs: [{ id: "all", name: "Все объекты", count: count_all_objects }],
+      contents: [{ id: "all", objects: content_objects }]
+    }
+
+    @realties.each do |type, objects|
+      @realty_data[:tabs] << { id: type.parameterize, name: type, count: objects.size }
+      @realty_data[:contents] << { id: type.parameterize, objects: objects }
+    end
+
+    @reviews = @team_member.reviews.published
+
+    add_breadcrumb meta_data.page_name, :team_members_path
+    add_breadcrumb @team_member.whole_name, :team_member_path
   end
 end
