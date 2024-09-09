f24df8822011115500a417e7d3bd083efdcd585c
Ruslan Fedotov <progruson@gmail.com>
2024-09-09 13:20:24 +0700

    Add main page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,15 @@ class ApplicationController < ActionController::Base
   layout :layout
   add_breadcrumb "Главная", :root_path
 
+  def index
+    @partners = Partner.active.sort_by { |partner| partner.position }
+    @team_members = TeamMember.active.sort_by { |team_member| team_member.position }
+
+    realties = Realty.active.joins(:realty_category).where(realty_category: { name: "Продажа" })
+    total_entries = realties.size > 18 ? 18 : realties.size
+    @realties = realties.paginate(page: params[:page], per_page: 6, total_entries: total_entries).order(created_at: :desc)
+  end
+
   private
 
   def layout
