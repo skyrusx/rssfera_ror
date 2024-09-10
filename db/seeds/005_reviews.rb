a1f7fc3b71ffe8b200fd14302a88e50fcd29aa4d
Ruslan Fedotov <progruson@gmail.com>
2024-09-10 12:37:47 +0700

    Fix seeds reviews

--- db/seeds/005_reviews.rb
+++ db/seeds/005_reviews.rb
@@ -51,7 +51,7 @@ reviews.each.with_index(1) do |review_data, index|
                    when 5 then "Новожилов"
                    when 6 then "Захарова"
                    when 7 then "Медведева"
-                   else "Павлова"
+                   else "Стрелец"
                    end
 
     team_member = TeamMember.find_by_last_name(tm_last_name)
