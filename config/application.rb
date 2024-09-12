fbd3fac2a01829f7fe4f1a5f191f38797464a0a6
Ruslan Fedotov <progruson@gmail.com>
2024-09-12 14:47:22 +0700

    Add time zone

--- config/application.rb
+++ config/application.rb
@@ -36,6 +36,9 @@ module Rssfera
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
 
+    config.time_zone = "Moscow"
+    config.active_record.default_timezone = :local
+
     # Don't generate system test files.
     config.generators.system_tests = nil
     config.i18n.default_locale = :ru
