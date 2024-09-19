c58890daf75206c1a51dadb01d83dc3544485aa1
Ruslan Fedotov <progruson@gmail.com>
2024-09-19 20:22:11 +0700

    Fix sells controller

--- app/controllers/sells_controller.rb
+++ app/controllers/sells_controller.rb
@@ -12,11 +12,22 @@ class SellsController < ApplicationController
     add_breadcrumb breadcrumb, :sells_path
 
     @current_realty = Realty::MENU_TYPES[params[:realty_id].to_i]
+    @current_realty_type = Realty::TYPES[params[:realty_id].to_i]
+
     @consultation = Message.new
     @message = Message.new
   end
 
   def search_buyers
     add_breadcrumb I18n.t("app.breadcrumbs.sells_search_buyers"), :sells_search_buyers_path
+    @search = params[:search]
+
+    if @search.present?
+      @realties = Realty.active.where(realty_category_id: 2)
+      @realties = @realties.where(type_object: @search[:type_object]) if @search[:type_object].present?
+      @realties = @realties.where(price: @search[:price][:start].to_i..@search[:price][:end].to_i) if @search[:price].present?
+    else
+      @realties = Realty.active.where(realty_category_id: 2)
+    end
   end
 end
