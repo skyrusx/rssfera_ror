54d2603c04113d11213aac1da57e140485609ec0
Ruslan Fedotov <progruson@gmail.com>
2024-09-22 17:51:01 +0700

    Fix RealtiesController show

--- app/controllers/realties_controller.rb
+++ app/controllers/realties_controller.rb
@@ -9,10 +9,14 @@ class RealtiesController < ApplicationController
     add_breadcrumb realty_page_name, path
 
     @team_member = TeamMember.find_by(id: @realty.team_member_id)
+    price_range = if @realty.realty_category.slug == "rent"
+                    @realty.price - 5000.0..@realty.price + 5000.0
+                  else
+                    @realty.price - 500000.0..@realty.price + 500000.0
+                  end
     @similar_realties = {
       by_price: Realty.where.not(id: @realty.id)
-                      .where(realty_category_id: @realty.realty_category_id,
-                             price: @realty.price - 5000.0..@realty.price + 5000.0),
+                      .where(realty_category_id: @realty.realty_category_id, price: price_range),
       by_location: Realty.where.not(id: @realty.id)
                          .where(realty_category_id: @realty.realty_category_id, district_id: @realty.district_id)
     }
