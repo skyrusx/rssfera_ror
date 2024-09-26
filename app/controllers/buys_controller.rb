c0814eb8ce4dc15328b9f2ee751968eb5cb40de5
Ruslan Fedotov <progruson@gmail.com>
2024-09-26 10:18:02 +0700

    Fix filter buys

--- app/controllers/buys_controller.rb
+++ app/controllers/buys_controller.rb
@@ -18,6 +18,43 @@ class BuysController < ApplicationController
 
     @category = RealtyCategory.find_by(slug: "buy")
     @realties = Realty.active.where(realty_category_id: @category.id, type_object: params[:realty_id]).order(:created_at)
+    @search = params[:search] || {}
+    if @search.present?
+      @search = @search.reject { |_, v| v.empty? }
+      @districts = @search[:city].present? ? City.find_by(id: @search[:city]).districts.pluck(:name, :id) : []
+      @streets = @search[:district].present? ? District.find_by(id: @search[:district]).streets.pluck(:name, :id) : []
+
+      filtering_params(@search).each do |key, value|
+        value = value.split(",") if key == "number_rooms"
+        @realties = @realties.public_send("filter_by_#{key}", value) if value.present?
+      end
+    else
+      @districts = []
+      if params[:city_id].present?
+        @districts = City.find(params[:city_id]).districts
+
+        if request.xhr?
+          respond_to do |format|
+            format.json {
+              render json: {districts: @districts}
+            }
+          end
+        end
+      end
+
+      @streets = []
+      if params[:district_id].present?
+        @streets = District.find(params[:district_id]).streets
+
+        if request.xhr?
+          respond_to do |format|
+            format.json {
+              render json: {streets: @streets}
+            }
+          end
+        end
+      end
+    end
   end
 
   def base
@@ -25,5 +62,11 @@ class BuysController < ApplicationController
 
     @message_extended = Message.new
     @message_short = Message.new
+  end
+
+  private
+
+  def filtering_params(params)
+    params.slice(:type_object, :price, :total_area, :floor, :city, :district, :street, :number_rooms)
   end
 end
