a85e2c44f1e88a265546f802d2989bd6724656b0
Ruslan Fedotov <progruson@gmail.com>
2024-10-09 11:34:21 +0700

    Fix buyers

--- app/controllers/sells_controller.rb
+++ app/controllers/sells_controller.rb
@@ -20,14 +20,52 @@ class SellsController < ApplicationController
 
   def search_buyers
     add_breadcrumb I18n.t("app.breadcrumbs.sells_search_buyers"), :sells_search_buyers_path
-    @search = params[:search]
 
+    @category = RealtyCategory.find_by(slug: "sell")
+    @realties = Realty.active.where(realty_category_id: @category.id).order(:created_at)
+
+    @search = params[:search] || {}
     if @search.present?
-      @realties = Realty.active.where(realty_category_id: 2)
-      @realties = @realties.where(type_object: @search[:type_object]) if @search[:type_object].present?
-      @realties = @realties.where(price: @search[:price][:start].to_i..@search[:price][:end].to_i) if @search[:price].present?
+      @search = @search.reject { |_, v| v.empty? }
+      @districts = @search[:city].present? ? City.find_by(id: @search[:city]).districts.pluck(:name, :id) : []
+      @streets = @search[:district].present? ? District.find_by(id: @search[:district]).streets.pluck(:name, :id) : []
+
+      filtering_params(@search).each do |key, value|
+        value = value.split(",") if key == "number_rooms"
+        @realties = @realties.public_send("filter_by_#{key}", value) if value.present?
+      end
     else
-      @realties = Realty.active.where(realty_category_id: 2)
+      @districts = []
+      if params[:city_id].present?
+        @districts = City.find(params[:city_id]).districts
+
+        if request.xhr?
+          respond_to do |format|
+            format.json {
+              render json: {districts: @districts}
+            }
+          end
+        end
+      end
+
+      @streets = []
+      if params[:district_id].present?
+        @streets = District.find(params[:district_id]).streets
+
+        if request.xhr?
+          respond_to do |format|
+            format.json {
+              render json: {streets: @streets}
+            }
+          end
+        end
+      end
     end
+  end
+
+  private
+
+  def filtering_params(params)
+    params.slice(:type_object, :price, :total_area, :floor, :city, :district, :street, :number_rooms)
   end
 end
