36329d2c24606dd0403b2579a7e2b5a8e358aa1f
Ruslan Fedotov <progruson@gmail.com>
2024-12-03 02:07:08 +0700

    Fix best-agent

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,10 +6,12 @@ class ApplicationController < ActionController::Base
     @partners = Partner.active.sort_by { |partner| partner.position }
     @team_members = TeamMember.active.sort_by { |team_member| team_member.position }
     @best_agent = TeamMember.active.find_by(best_agent: true)
+    @best_agent_realties = @best_agent.realties.active.joins(:realty_category).where(realty_category: { slug: %w[buy rent] }).group_by(&:type_object).to_h.map { |_, values| values }.flatten
 
     realties = Realty.active.joins(:realty_category).where(realty_category: { slug: %w[buy rent] })
     total_entries = realties.size > 18 ? 18 : realties.size
     @realties = realties.paginate(page: params[:page], per_page: 6, total_entries: total_entries).order(created_at: :desc)
+    @message = Message.new
   end
 
   private
