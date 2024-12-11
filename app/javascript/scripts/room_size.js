a58ee669c1be5807fafda9ba99a72f9eb9740d0e
Ruslan Fedotov <progruson@gmail.com>
2024-12-11 16:52:02 +0700

    Fix room size

--- app/javascript/scripts/room_size.js
+++ app/javascript/scripts/room_size.js
@@ -1,20 +1,22 @@
 $(document).on('turbo:load', function() {
-    let numZero = "";
-    let numOne = "";
-    let rooms = {"one": "1", "two": "2", "three": "3", "four": "4", "five": "5"}
-    let oldNumberRooms = $("#number-rooms").val().split("");
+    if ($("#number-rooms").length > 0) {
+        let numZero = "";
+        let numOne = "";
+        let rooms = {"one": "1", "two": "2", "three": "3", "four": "4", "five": "5"}
+        let oldNumberRooms = $("#number-rooms").val().split("");
 
-    $(".btn-check").change(function() {
-        if($(this).prop('checked')) {
-            numOne = $(this)[0].id.split("-")[0];
-            oldNumberRooms.push(rooms[numOne].toString());
-            $("#number-rooms").val(oldNumberRooms.sort());
-        } else {
-            numZero = $(this)[0].id.split("-")[0];
-            oldNumberRooms = $.grep(oldNumberRooms, function(value) {
-                return value != rooms[numZero].toString();
-            });
-            $("#number-rooms").val(oldNumberRooms.sort());
-        }
-    });
+        $(".btn-check").change(function() {
+            if($(this).prop('checked')) {
+                numOne = $(this)[0].id.split("-")[0];
+                oldNumberRooms.push(rooms[numOne].toString());
+                $("#number-rooms").val(oldNumberRooms.sort());
+            } else {
+                numZero = $(this)[0].id.split("-")[0];
+                oldNumberRooms = $.grep(oldNumberRooms, function(value) {
+                    return value != rooms[numZero].toString();
+                });
+                $("#number-rooms").val(oldNumberRooms.sort());
+            }
+        });
+    }
 });
